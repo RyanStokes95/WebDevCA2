39a5e070355b04a1129cb4ed095538da52ab92ac
RyanStokes95 <ryanstokes560@gmail.com>
2024-07-10 22:46:04 +0100

    Mongo package installation and DB Config creation

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+const server = app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+server.on('error', (error) => {
+    console.error(`Error starting the server: ${error.message}`);
 });
