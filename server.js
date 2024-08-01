fdcb4ef73ed1030cd5599fc435121ff89bd59cc2
RyanStokes95 <ryanstokes560@gmail.com>
2024-08-01 16:22:44 +0100

    Comments added to server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ Created - 18/07/24
 Last Modified - 31/07/24
 */
 
+//imports and dependencies
 require('dotenv').config();
 const { connectDB } = require('./dbconfig');
 const express = require('express');
@@ -15,9 +16,11 @@ const User = require('./schemas/User')
 const port = process.env.PORT;
 const app = express();
 
+//Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
+//Server Connection and error handling
 const server = app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
@@ -26,9 +29,12 @@ server.on('error', (error) => {
     console.error(`Error starting the server: ${error.message}`);
 });
 
+//Coonect to Mongo DB
 connectDB();
 
+//User Register Route
 app.post('/user-register', async (req, res) => {
+    //Request Body - User Details
     const {
         username, 
         password, 
@@ -42,10 +48,11 @@ app.post('/user-register', async (req, res) => {
         country} = req.body;
 
         try {
+            //BCrypt used to hash and salt password before DB insertion
             const salt = await bcrypt.genSalt(10);
-            
             const hashedPassword = await bcrypt.hash(password, salt);
-                
+            
+            //Data used to create new user schema object
             const formData = new User({
                 username,
                 password: hashedPassword,
@@ -58,29 +65,36 @@ app.post('/user-register', async (req, res) => {
                 city,
                 country
             });
-    
+            
+            //Success if data is stored in mongoDB
             await formData.save();
             res.status(200).send("User Submitted Successfully");
         } catch (error) {
+            //Error message if user is not added correctly
             res.status(500).send("Failed to Add User");
         }
     });
 
-// Login endpoint
+// Login Route
 app.post('/login', async (req, res) => {
+    //username and password request body
     const { username, password } = req.body;
-
+    //if username and hashed password match a DB record user is logged in
     try {
+        //Check username
         const user = await User.findOne({ username });
         if (!user) {
             return res.status(401).send("Invalid Username or Password");
         }
 
+        //Check password using BCrypt
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
             return res.status(401).send('Invalid Username or Password');
         }
+        //Response of true if there is a match found
         res.json({ success: true });
+    //Error handling
     } catch (error) {
         console.error("Error Cannot Login", error);
         res.status(500).send("Server Error");
